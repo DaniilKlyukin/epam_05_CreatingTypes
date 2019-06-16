035d30050dbad46bc2c297a0d679511cef347c56
Daniil <anatoliikljukin@mail.ru>
2019-06-17 00:33:23 +0400

    Create IOrderable.cs
    Added interface for strategy pattern implementation.

---
+++ TasksLibrary/TasksLibrary/IOrderable.cs
@@ -0,0 +1,13 @@
+﻿namespace TasksLibrary
+{
+    public enum Direction
+    {
+        Ascending = 1,
+        Descending = -1
+    }
+
+    public interface IOrderable
+    {
+        void Order(int[,] arr, Direction d);
+    }
+}
