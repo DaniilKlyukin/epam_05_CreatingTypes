90ddc6b0194a3f4d941ece26784d1291233a9aba
Daniil <anatoliikljukin@mail.ru>
2019-06-17 00:42:50 +0400

    Update CreatingTypesTests.cs
    Added tests for 2nd task: ordering test by sum, min/max element in row.

--- CreatingTypes/CreatingTypesTests.cs
+++ CreatingTypes/CreatingTypesTests.cs
@@ -50,5 +50,161 @@ namespace CreatingTypes
             Assert.AreEqual(0.5, tWorker.FindNthRoot(0.5, 1, 0.1), 0.1);
             Assert.AreEqual(1.77245, tWorker.FindNthRoot(Math.PI, 2, 0.01), 0.01);
         }
+
+        [Test]
+        public void CheckBubbleSortBySum()
+        {
+            var testData = new BubbleSortTestData[]
+            {
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {1,2 },
+                        {3,4 },
+                        {0,5 }
+                    },
+                    OrderMethod = new OrderByRowSum(),
+                    Direction = Direction.Descending,
+                    ExpectedArray = new int[,]
+                    {
+                        {3,4 },
+                        {0,5 },
+                        {1,2 }
+                    }
+                },
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {1,2 },
+                        {3,4 },
+                        {0,5 }
+                    },
+                    OrderMethod = new OrderByRowSum(),
+                    Direction = Direction.Ascending,
+                    ExpectedArray = new int[,]
+                    {
+                        {1,2 },
+                        {0,5 },
+                        {3,4 }
+                    }
+                }
+            };
+
+            MakeTests(testData);
+        }
+
+        [Test]
+        public void CheckBubbleSortByMaxElement()
+        {
+            var testData = new BubbleSortTestData[]
+            {
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {1,2,30 },
+                        {3,4,50 },
+                        {0,5,49 }
+                    },
+                    OrderMethod = new OrderByRowMaxElement(),
+                    Direction = Direction.Descending,
+                    ExpectedArray = new int[,]
+                    {
+                        {3,4,50 },
+                        {0,5,49 },
+                        {1,2,30 }
+                    }
+                },
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {-1,2,1,4 },
+                        {-3,4,3,4 },
+                        {0,2,-1,4 }
+                    },
+                    OrderMethod = new OrderByRowMaxElement(),
+                    Direction = Direction.Descending,
+                    ExpectedArray = new int[,]
+                    {
+                        {-1,2,1,4 },
+                        {-3,4,3,4 },
+                        {0,2,-1,4 }
+                    }
+                }
+            };
+
+            MakeTests(testData);
+        }
+
+        [Test]
+        public void CheckBubbleSortByMinElement()
+        {
+            var testData = new BubbleSortTestData[]
+            {
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {1,2 },
+                        {10,20 },
+                        {-50,1 },
+                        {32,32 },
+                        {0,1 },
+                        {1,3 }
+                    },
+                    OrderMethod = new OrderByRowMinElement(),
+                    Direction = Direction.Descending,
+                    ExpectedArray = new int[,]
+                    {
+                        {32,32 },
+                        {10,20 },
+                        {1,2 },
+                        {1,3 },
+                        {0,1 },
+                        {-50,1 }
+                    }
+                },
+                new BubbleSortTestData()
+                {
+                    ActualArray = new int[,]
+                    {
+                        {1},
+                        {3},
+                        {0}
+                    },
+                    OrderMethod = new OrderByRowMinElement(),
+                    Direction = Direction.Ascending,
+                    ExpectedArray = new int[,]
+                    {
+                        {0},
+                        {1},
+                        {3}
+                    }
+                }
+            };
+
+            MakeTests(testData);
+        }
+
+        private void MakeTests(BubbleSortTestData[] testData)
+        {
+            foreach (var t in testData)
+            {
+                var array = t.ActualArray;
+                tWorker.BubbleSort(array, t.OrderMethod, t.Direction);
+                CollectionAssert.AreEqual(t.ExpectedArray, array);
+            }
+        }
+    }
+
+    class BubbleSortTestData
+    {
+        public int[,] ActualArray { get; set; }
+        public int[,] ExpectedArray { get; set; }
+        public IOrderable OrderMethod { get; set; }
+        public Direction Direction { get; set; }
     }
 }
