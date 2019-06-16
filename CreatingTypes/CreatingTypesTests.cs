b7cc9ef4dc321856aedd719577e11cdc4e60d82c
Daniil <anatoliikljukin@mail.ru>
2019-06-16 20:48:50 +0400

    Create CreatingTypesTests.cs
    Added tests for 1st task: common tests and argument exception tests.

---
+++ CreatingTypes/CreatingTypesTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using MSUnitTest = Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using TasksLibrary;
+
+namespace CreatingTypes
+{
+    using System.Linq;
+    using MSUnitTestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+    [TestFixture]
+    [MSUnitTest.TestClass]
+    public class CreatingTypesTests
+    {
+        public MSUnitTestContext TestContext { get; set; }
+        TaskWorker tWorker = new TaskWorker();
+
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        [TestCase(0, 2, 0.00000001, 0)]
+        public void CheckFindingNthRoot(double number, int n, double eps, double expected)
+        {
+            var actual = tWorker.FindNthRoot(number, n, eps);
+
+            Assert.AreEqual(expected, actual, eps);
+        }
+
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.01, -2, 0.0001)]
+        [TestCase(0.01, 2, -1)]
+        [TestCase(-1, 0, 0)]
+        [TestCase(81, 2, 0)]
+        public void CheckFindingNthRootExceptions(double number, int n, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => tWorker.FindNthRoot(number, n, eps));
+        }
+    }
+}
