a1bb71d8d1c9614590a373ba0457d769f99d98cf
Daniil <anatoliikljukin@mail.ru>
2019-06-18 13:22:57 +0400

    fix Order.cs
    When the maximum (minimum) is equal, the comparison with the next order maximum occurs.

--- TasksLibrary/TasksLibrary/Order.cs
+++ TasksLibrary/TasksLibrary/Order.cs
@@ -2,11 +2,11 @@
 {
     using System.Linq;
 
-    public delegate int RowEvaluator(int[,] arr, int row);
+    public delegate int[] RowEvaluator(int[,] arr, int row);
 
     public abstract class Order
     {
-        public void SwapRows(int[,] array, int row1, int row2)
+        protected void SwapRows(int[,] array, int row1, int row2)
         {
             for (int i = 0; i < array.GetLength(1); i++)
             {
@@ -16,7 +16,7 @@
             }
         }
 
-        public RowInfo[] CalculateInfo(int[,] arr, RowEvaluator evaluate)
+        protected RowInfo[] CalculateInfo(int[,] arr, RowEvaluator evaluate)
         {
             var rows = arr.GetLength(0);
             var columns = arr.GetLength(1);
@@ -29,15 +29,31 @@
             return rowsInfo;
         }
 
-        public void BubbleSort(int[,] arr, RowInfo[] rowsInfo, Direction d)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="rowsInfo"></param>
+        /// <param name="d"></param>
+        protected void BubbleSort(int[,] arr, RowInfo[] rowsInfo, Direction d)
         {
             var rows = arr.GetLength(0);
             var columns = arr.GetLength(1);
 
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < rows; j++)
-                    if (rowsInfo[i].RowEigenvalue.CompareTo(rowsInfo[j].RowEigenvalue) == -(int)d)
-                        Swap(rowsInfo, i, j);
+            for (int i = 1; i < rows; i++)
+                for (int j = 0; j < i; j++)
+                {
+                    for (int v = 0; v < columns; v++)
+                    {
+                        var condition = rowsInfo[j].RowEigenvalues[v].CompareTo(rowsInfo[i].RowEigenvalues[v]);
+
+                        if (condition == (int)d)
+                            Swap(rowsInfo, i, j);
+
+                        if (condition != 0)
+                            break;
+                    }
+                }
 
             for (int i = 0; i < rows; i++)
                 if (rowsInfo[i].RowIndex != i)
@@ -52,82 +68,112 @@
                 }
         }
 
+        protected void BubbleSort(int[] arr, Direction d)
+        {
+            for (int i = 1; i < arr.Length; i++)
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j].CompareTo(arr[i]) == (int)d)
+                        Swap(arr, i, j);
+                }
+        }
+
         private void Swap<T>(T[] arr, int i, int j)
         {
             T temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
-    }
 
-    public class RowInfo
-    {
-        public int RowIndex { get; set; }
-        public int RowEigenvalue { get; set; }
-
-        public RowInfo(int index, int value)
+        protected class RowInfo
         {
-            RowIndex = index;
-            RowEigenvalue = value;
+            public int RowIndex { get; set; }
+            public int[] RowEigenvalues { get; set; }
+
+            public RowInfo(int index, int[] value)
+            {
+                RowIndex = index;
+                RowEigenvalues = value;
+            }
         }
     }
 
     public class OrderByRowSum : Order, IOrderable
     {
+        /// <summary>
+        /// Arranges the rows of the matrix by the sum elements in rows.
+        /// </summary>
+        /// <param name="arr">Input matrix.</param>
+        /// <param name="d">Ordering Method (Ascending / Descending).</param>
         public void Order(int[,] arr, Direction d)
         {
             var info = CalculateInfo(arr, RowSum);
             BubbleSort(arr, info, d);
         }
 
-        private int RowSum(int[,] arr, int row)
+        private int[] RowSum(int[,] arr, int row)
         {
-            var sum = 0;
+            var columnsCount = arr.GetLength(1);
+            var sum = new int[columnsCount];
 
-            for (int i = 0; i < arr.GetLength(1); i++)
-                sum += arr[row, i];
+            for (int i = 0; i < columnsCount; i++)
+                sum[0] += arr[row, i];
 
-            return sum;
+            return sum.Select(x => sum[0]).ToArray();
         }
     }
 
     public class OrderByRowMaxElement : Order, IOrderable
     {
+        /// <summary>
+        /// Arranges the rows of the matrix by the maximum elements in rows.
+        /// </summary>
+        /// <param name="arr">Input matrix.</param>
+        /// <param name="d">Ordering Method (Ascending / Descending).</param>
         public void Order(int[,] arr, Direction d)
         {
             var info = CalculateInfo(arr, RowMax);
             BubbleSort(arr, info, d);
         }
 
-        private int RowMax(int[,] arr, int row)
+        private int[] RowMax(int[,] arr, int row)
         {
-            var max = int.MinValue;
+            var columnsCount = arr.GetLength(1);
+            var maximums = new int[columnsCount];
 
-            for (int i = 0; i < arr.GetLength(1); i++)
-                if (max < arr[row, i])
-                    max = arr[row, i];
+            for (int i = 0; i < columnsCount; i++)
+                maximums[i] = arr[row, i];
 
-            return max;
+            BubbleSort(maximums, Direction.Descending);
+
+            return maximums;
         }
     }
 
     public class OrderByRowMinElement : Order, IOrderable
     {
+        /// <summary>
+        /// Arranges the rows of the matrix by the minimum elements in rows.
+        /// </summary>
+        /// <param name="arr">Input matrix.</param>
+        /// <param name="d">Ordering Method (Ascending / Descending).</param>
         public void Order(int[,] arr, Direction d)
         {
             var info = CalculateInfo(arr, RowMin);
             BubbleSort(arr, info, d);
         }
 
-        private int RowMin(int[,] arr, int row)
+        private int[] RowMin(int[,] arr, int row)
         {
-            var min = int.MaxValue;
+            var columnsCount = arr.GetLength(1);
+            var minimums = new int[columnsCount];
 
-            for (int i = 0; i < arr.GetLength(1); i++)
-                if (arr[row, i] < min)
-                    min = arr[row, i];
+            for (int i = 0; i < columnsCount; i++)
+                minimums[i] = arr[row, i];
 
-            return min;
+            BubbleSort(minimums, Direction.Ascending);
+
+            return minimums;
         }
     }
 }
